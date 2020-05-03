8eac9a2cfadf62049b63a9e47dcb0b822191d8d5
Nodarida <56589980+Nodarida@users.noreply.github.com>
2020-05-03 13:27:32 +0200

    Vue_first_project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Plan msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Plan from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  // eslint-disable-next-line vue/no-unused-components
+  Plan
   }
 }
 </script>
---
+++ src/views/Plan.vue
@@ -0,0 +1,43 @@
+
+<template>
+<h1>Mensa Foooood</h1>
+  <select name="Wochentage" id="selectBox">
+
+    <option>Montag</option>
+    <option>Dienstag</option>
+    <option>Mittwoch</option>
+    <option>Donnerstag</option>
+    <option>Freitag</option>
+
+  </select>
+  <br>
+  <br>
+  <br>
+
+  <table>
+    <tr>
+      <th>Information</td>
+      <th>Kategorie</td>
+      <th>Kosten</td>
+      <th>Tag</td>
+      <th>Downvotes</td>
+      <th>ID</td>
+      <th>Labels</td>
+      <th>Upvotes</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'Plan',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
